e3486794afe0dbcec75d15876640ec5f916f6f0d
Maciek Danik <maciekdanik@gmail.com>
2024-03-01 07:56:11 +0100

    empty source file

---
+++ Net_Java_Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
